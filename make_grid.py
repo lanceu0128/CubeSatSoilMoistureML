b2ee3c6a2c0e8fd7a1e5b725dbf04c5134a80821
Lance <lanceu0128@gmail.com>
2024-09-15 22:00:28 -0400

    better make_grid.py logic and comments

--- make_grid.py
+++ make_grid.py
@@ -1,64 +1,72 @@
 import netCDF4, os, glob, pickle
+from pathlib import Path
 from pprint import pprint
 import numpy as np
 
-# Task: make a SNR and Reflectivity grid at 5km resolution (7200 in longitude and 3600 in latitude)
-# conversions between lon/x and lat/y (assuming we need to convert): 
-# # lon = (x * 0.05) - 180 
-# # lat = (y * 0.05) - 90
-# # x = (lon + 180) / 0.05 
-# # y = (lat + 90) / 0.05
+# Task: make SNR and Reflectivity grids at 5km resolution (7200 in longitude and 3600 in latitude)
 
-# initializing grid
-snr_grid = np.zeros((3600, 7200))
-reflectivity_grid = np.zeros((3600, 7200))
+# reads a given NetCDF4 file and updates the SNR and Reflectivity grid arguments with the values
+def read_file(file, grid_snr, grid_reflectivity):
+    nc_file = netCDF4.Dataset(file, "r", format="NETCDF4")
 
-max_lon = 45
-min_lon = 45
-max_lat = 45
-min_lat = 45
-
-def read_file(file_path, snr_grid, reflectivity_grid):
-    global max_lon, min_lon, max_lat, min_lat
-    nc_file = netCDF4.Dataset(file_path, "r", format="NETCDF4")
-
-    # reading relevant file variables
+    # variables in 1D arrays; we use the lon and lat values to locate the Reflectivity and SNR values 
     reflectivity = nc_file.variables['reflectivity_at_sp'][:]
     snr = nc_file.variables['reflect_snr_at_sp'][:]
-    lon = nc_file.variables['sp_lon'][:] 
-    lat = nc_file.variables['sp_lat'][:] 
-
-    max_lon = max(max_lon, max(lon)-180)
-    min_lon = min(min_lon, min(lon)-180)
-    max_lat = max(max_lat, max(lat))
-    min_lat = min(min_lat, min(lat))
+    lon = nc_file.variables['sp_lon'][:]
+    lat = nc_file.variables['sp_lat'][:]
 
     for i in range(len(reflectivity)):
-        x = int((lon[i] - 180) / 0.05)
-        y = int(lat[i] / 0.05)
+        # the lon and lat values in the files range from [0, 360] and [-90, 90] respectively
+        # we convert to [0, 360] and [0, 180] first to avoid negatives being interpreted incorrectly as XY
+        # when creating the graph we interpret the coordinates as [-180, 180] and [-90, 90]
+        x = int((lon[i]) / 0.05)
+        y = int((lat[i] + 90) / 0.05)
 
-        print(lon[i]-180, lat[i], snr[i])
+        # the prior conversions allow for 7200 and 3600 coordinates which don't fit into the grid 
+        x = max(min(x, 7199), 0)
+        y = max(min(y, 3599), 0)
 
-        snr_grid[y][x] = snr[i] # y prior to x because python accesses rows before columns
-        reflectivity_grid[y][x] = reflectivity[i]
+        # the data contains masked and NaN values that we want to avoid 
+        if not np.ma.is_masked(snr[i]) and not np.isnan(snr[i]):
+            # y comes before x because python arrays are accessed with rows before cols
+            grid_snr[y][x] = snr[i]
+
+        if not np.ma.is_masked(reflectivity[i]) and not np.isnan(reflectivity[i]):
+            grid_reflectivity[y][x] = reflectivity[i]
 
     nc_file.close()
+    
+    return grid_snr, grid_reflectivity
 
 if __name__ == "__main__":
-    directory = '/data01/jyin/SPIRE/RAW/2024/20240610'
+    # this folder contains all of our data in subfolders for a given date (YYYYMMDD)
+    directory = Path('/data01/jyin/SPIRE/RAW/2024')
+    # the month we want to grab data for
+    month = '202404'
 
-    for root, dirs, files in os.walk(directory):
-        for i, file in enumerate(files):
-            if file.endswith('.nc'):
-                if (i % 100 == 0):
-                    print(f"Reading file {i}")            
-                file_path = os.path.join(root, file)
-                read_file(file_path, snr_grid, reflectivity_grid)    
+    # default values are -9999 so that we can identify unfilled values later easily
+    snr_grid = np.full((3600, 7200), -9999)
+    reflectivity_grid = np.full((3600, 7200), -9999)
 
-    snr_grid = np.nan_to_num(snr_grid)
-    reflectivity_grid = np.nan_to_num(reflectivity_grid)
-    print(max_lon, min_lon, max_lat, min_lat)
-        
+    # walk through every date folder and process anything from given month
+    for i, folder in enumerate(directory.iterdir()):
+        if folder.is_dir():
+            if folder.name.startswith(month):
+                print(f"Processing date: {folder.name}")
+                # each date folder has numerous NetCDF4 files that are to be combined for our final grid
+                for j, file in enumerate(folder.iterdir()):
+                    if file.is_file() and file.suffix == '.nc':        
+                        if (j % 250 == 0):
+                            print(f"- Reading file #{j}")
+                            print(f"- SNR Values above 0: {np.sum(snr_grid > 0)}")  
+
+                        snr_grid, reflectivity_grid = read_file(file, snr_grid, reflectivity_grid)  
+
+    # probably not necessary because we check for non-numeric values in read_file(), but good practice
+    snr_grid = np.nan_to_num(snr_grid, nan=-9999)
+    reflectivity_grid = np.nan_to_num(reflectivity_grid, nan=-9999)
+
+    # pickle files are nice
     with open('/data01/lpu/snr_grid.pkl', 'wb') as f:
         pickle.dump(snr_grid, f)
     with open('/data01/lpu/reflectivity_grid.pkl', 'wb') as f:
--- visualize_grid.py
+++ visualize_grid.py
@@ -3,18 +3,26 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import pickle
 
+# creates a matplotlib figure for a given grid over an Earth map
 def create_figure(grid, title, label, file_name, isomin, isomax):
     fig, ax = plt.subplots(figsize=(12, 8))
-    m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90,
-                llcrnrlon=-180, urcrnrlon=180, resolution='c', ax=ax)
 
-    lon_vals = np.linspace(-180, 180, grid.shape[1])
+    # the Basemap library adds a map of the Earth for easy viewing
+    m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90,
+                llcrnrlon=0, urcrnrlon=360, resolution='c', ax=ax)
+
+    # creates a meshgrid from [0, 360] and [-90, 90] used to translate argument grid to lon and lat values 
+    lon_vals = np.linspace(0, 360, grid.shape[1])
     lat_vals = np.linspace(-90, 90, grid.shape[0])
     lon_grid, lat_grid = np.meshgrid(lon_vals, lat_vals)
 
-    cs = m.pcolormesh(lon_grid, lat_grid, grid, shading='auto', 
+    # remove values that are equal to -9999 (the default value in make_grid.py)
+    masked_grid = np.ma.masked_where(grid == -9999, grid)
+
+    cs = m.pcolormesh(lon_grid, lat_grid, masked_grid, shading='auto', 
                     cmap='viridis', vmin=isomin, vmax=isomax)
 
+    # customization stuff
     m.drawcoastlines(linewidth=0.5)
     m.drawcountries(linewidth=0.5)
 
@@ -35,5 +43,5 @@ if __name__ == "__main__":
     print(f"SNR Min: {np.min(snr_grid)}, Max: {np.max(snr_grid)}")
     print(f"Reflectivity Min: {np.min(reflectivity_grid)}, Max: {np.max(reflectivity_grid)}")
 
-    create_figure(snr_grid, "SNR 06-08-2024", "SNR (dB)", "snr_map.png", 0.00001, 10)
-    create_figure(reflectivity_grid, "Reflectivity 06-08-2024", "Reflectivity (dBZ)", "reflectivity_map.png", 0.00001, 0.01)
+    # create_figure(snr_grid, "SNR 06-2024", "SNR (dB)", "snr_map.png", 0, 10)
+    create_figure(reflectivity_grid, "Reflectivity 06-2024", "Reflectivity (dBZ)", "reflectivity_map.png", -20, 20)
