d2e5f6c04c289ebf4db0d07713262341cad0f1ae
Lance <lanceu0128@gmail.com>
2024-09-21 14:56:24 -0400

    investigating issues with reflectivity; found a lot of overlapping data

--- make_grid_CYGNSS.py
+++ make_grid_CYGNSS.py
@@ -6,11 +6,12 @@ import pickle
 # Task: make a grid of CYGNSS SNR and Reflectivity data for comparison and validation of SPIRE data
 # very few comments because the logic is almost exactly the same as make_grid.py  
 
-def read_file(file, grid_snr, grid_reflectivity):
+def read_file(file, grid_snr, grid_refl, grid_count_snr, grid_count_refl):
     nc_file = netCDF4.Dataset(file, "r", format="NETCDF4")
     
     # lon ~ [0, 360], lat ~ [-45, 45]
-    reflectivity = nc_file.variables['reflectivity_peak'][:]
+    # [:] needed to convert from netCDF4 variable arrays to float arrays
+    refl = nc_file.variables['reflectivity_peak'][:]
     snr = nc_file.variables['ddm_snr'][:]
     lon = nc_file.variables['sp_lon'][:]
     lat = nc_file.variables['sp_lat'][:]
@@ -19,39 +20,47 @@ def read_file(file, grid_snr, grid_reflectivity):
     y = ((lat + 45) / 0.05).astype(int) # about half of the range
 
     x = np.clip(x, 0, 7199)
-    y = np.clip(y, 0, 1799)
 
     valid_snr = ~np.ma.getmaskarray(snr) & ~np.isnan(snr)
-    valid_reflectivity = ~np.ma.getmaskarray(reflectivity) & ~np.isnan(reflectivity)
+    valid_refl = ~np.ma.getmaskarray(refl) & ~np.isnan(refl)
 
     grid_snr[y[valid_snr], x[valid_snr]] = snr[valid_snr]
+    grid_count_snr[y[valid_snr], x[valid_snr]] += 1
 
-    grid_reflectivity[y[valid_reflectivity], x[valid_reflectivity]] = reflectivity[valid_reflectivity]
+    grid_refl[y[valid_refl], x[valid_refl]] = refl[valid_refl]
+    grid_count_refl[y[valid_refl], x[valid_refl]] += 1
 
     nc_file.close()
     
-    return grid_snr, grid_reflectivity
-
+    return grid_snr, grid_refl, grid_count_snr, grid_count_refl
 
 if __name__ == "__main__":
     directory = Path('/data01/jyin/CYGNSS/data/V3.2/2023/')
-    month = "-e202304"
+    month = "-e202305"
 
     # lat ~ [-45, 45], so we need to have 1800 lat values in the grid to keep the same resolution
     snr_grid = np.full((1800, 7200), -9999)
-    reflectivity_grid = np.full((1800, 7200), -9999)
+    refl_grid = np.full((1800, 7200), -9999)
+    count_snr_grid = np.zeros((1800,7200))
+    count_refl_grid = np.zeros((1800,7200))
 
     for folder in directory.iterdir():
         if folder.is_dir():            
             for file in folder.iterdir():
                 if file.is_file() and file.suffix == '.nc' and month in file.name:
                     print(f"Reading file: {file.as_posix()}")
-                    snr_grid, reflectivity_grid = read_file(file, snr_grid, reflectivity_grid)
+                    snr_grid, refl_grid, count_snr_grid, count_refl_grid = read_file(file, snr_grid, refl_grid, count_snr_grid, count_refl_grid)
  
     snr_grid = np.nan_to_num(snr_grid, nan=-9999)
-    reflectivity_grid = np.nan_to_num(reflectivity_grid, nan=-9999)
+    refl_grid = np.nan_to_num(refl_grid, nan=-9999)
+    count_snr_grid = np.nan_to_num(count_snr_grid, nan=-9999)
+    count_refl_grid = np.nan_to_num(count_refl_grid, nan=-9999)
 
-    with open('/data01/lpu/snr_grid_cygnss.pkl', 'wb') as f:
+    with open('/data01/lpu/cygnss_snr_grid.pkl', 'wb') as f:
         pickle.dump(snr_grid, f)
-    with open('/data01/lpu/reflectivity_grid_cygnss.pkl', 'wb') as f:
-        pickle.dump(reflectivity_grid, f)
+    with open('/data01/lpu/cygnss_reflectivity_grid.pkl', 'wb') as f:
+        pickle.dump(refl_grid, f)
+    with open('/data01/lpu/cygnss_count_snr_grid.pkl', 'wb') as f:
+        pickle.dump(count_snr_grid, f)
+    with open('/data01/lpu/cygnss_count_reflectivity_grid.pkl', 'wb') as f:
+        pickle.dump(count_refl_grid, f)
--- visualize_grid.py
+++ visualize_grid.py
@@ -8,16 +8,16 @@ def create_figure(grid, title, label, file_name, isomin, isomax):
     fig, ax = plt.subplots(figsize=(12, 8))
 
     # the Basemap library adds a map of the Earth for easy viewing
-    m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90,
+    m = Basemap(projection='cyl', llcrnrlat=-45, urcrnrlat=45,
                 llcrnrlon=0, urcrnrlon=360, resolution='c', ax=ax)
 
     # creates a meshgrid from [0, 360] and [-90, 90] used to translate argument grid to lon and lat values 
     lon_vals = np.linspace(0, 360, grid.shape[1])
-    lat_vals = np.linspace(-90, 90, grid.shape[0])
+    lat_vals = np.linspace(-45, 45, grid.shape[0])
     lon_grid, lat_grid = np.meshgrid(lon_vals, lat_vals)
 
     # remove values that are equal to -9999 (the default value in make_grid.py)
-    masked_grid = np.ma.masked_where(np.logical_or(grid == -9999, grid == 0), grid)
+    masked_grid = np.ma.masked_where(grid == -9999, grid)
 
     cs = m.pcolormesh(lon_grid, lat_grid, masked_grid, shading='auto', 
                     cmap='viridis', vmin=isomin, vmax=isomax)
@@ -25,6 +25,9 @@ def create_figure(grid, title, label, file_name, isomin, isomax):
     # customization stuff
     m.drawcoastlines(linewidth=0.5)
     m.drawcountries(linewidth=0.5)
+
+    m.drawparallels(np.arange(-90, 91, 20), labels=[1, 0, 0, 0])  # Latitude lines
+    m.drawmeridians(np.arange(0, 361, 20), labels=[0, 0, 0, 1])   # Longitude lines
 
     cbar = m.colorbar(cs, location='right', pad="5%")
     cbar.set_label(label)
@@ -37,15 +40,27 @@ def create_figure(grid, title, label, file_name, isomin, isomax):
 if __name__ == "__main__":
     # with open('/data01/lpu/spire_snr_grid.pkl', 'rb') as f:
     #     spire_snr_grid = pickle.load(f)
-    with open('/data01/lpu/spire_reflectivity_grid.pkl', 'rb') as f:
-        spire_reflectivity_grid = pickle.load(f)
+    # with open('/data01/lpu/spire_reflectivity_grid.pkl', 'rb') as f:
+    #     spire_reflectivity_grid = pickle.load(f)
+
+    # CYGNSS DATA
     # with open('/data01/lpu/cygnss_snr_grid.pkl', 'rb') as f:
     #     cygnss_snr_grid = pickle.load(f)
-    with open('/data01/lpu/cygnss_reflectivity_grid.pkl', 'rb') as f:
-        cygnss_reflectivity_grid = pickle.load(f)
+    # with open('/data01/lpu/cygnss_reflectivity_grid.pkl', 'rb') as f:
+    #     cygnss_refl_grid = pickle.load(f)
+    with open('/data01/lpu/cygnss_count_snr_grid.pkl', 'rb') as f:
+        cygnss_count_snr_grid = pickle.load(f)
+    with open('/data01/lpu/cygnss_count_reflectivity_grid.pkl', 'rb') as f:
+        cygnss_count_refl_grid = pickle.load(f)
+
+    print(f"Size of Grid: {7200 * 1800}")
+    print(f"# of Values where SNR and Refl Count are the same: {np.count_nonzero((cygnss_count_refl_grid == cygnss_count_snr_grid) == True)}")
+    print(f"# of Values where SNR and Refl Count are different: {np.count_nonzero((cygnss_count_refl_grid == cygnss_count_snr_grid) == False)}")
 
     # create_figure(spire_snr_grid, "SPIRE SNR 04-2024", "SNR (dB)", "spire_snr_map.png", 0, 10)
-    create_figure(spire_reflectivity_grid, "SPIRE Reflectivity 04-2024", "Reflectivity (dBZ)", "spire_reflectivity_map.png", -20, 20)
+    # create_figure(spire_reflectivity_grid, "SPIRE Reflectivity 04-2024", "Reflectivity (dBZ)", "spire_reflectivity_map.png", -20, 20)
     # create_figure(cygnss_snr_grid, "CYGNSS SNR 04-2024", "SNR (dB)", "cygnss_snr_map.png", 0, 10)
-    create_figure(cygnss_reflectivity_grid, "CYGNSS Reflectivity 04-2024", "Reflectivity (dBZ)", "cygnss_reflectivity_map.png", -20, 20)
+    # create_figure(cygnss_refl_grid, "CYGNSS Reflectivity 04-2024", "Reflectivity (dBZ)", "cygnss_reflectivity_map.png", 0, 0.014)
+    create_figure(cygnss_count_snr_grid, "CYGNSS SNR Count 04-2024", "Count", "cygnss_count_snr_map.png", 0, 35)
+    create_figure(cygnss_count_refl_grid, "CYGNSS Reflectivity Count 04-2024", "Count", "cygnss_count_reflectivity_map.png", 0, 35)
     
