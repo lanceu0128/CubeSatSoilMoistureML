4e827987f5c59e28f9cd7984692538837b7ec871
Lance <lanceu0128@gmail.com>
2024-10-06 22:56:23 -0400

    zoomed in and better comparison maps

--- visualize_comparison.py
+++ visualize_comparison.py
@@ -22,38 +22,32 @@ def calculate_statistics(grid1, grid2):
 
     return diff_grid, stats
 
-# creates a matplotlib figure for a given grid over an Earth map
-def create_figure(grid, title, label, file_name, isomin, isomax, latmin, latmax, stats=None):
+def create_figure(grid, title, label, file_name, isomin, isomax, lonmin, lonmax, latmin, latmax, stats=None):
     fig, ax = plt.subplots(figsize=(12, 8))
 
-    # the Basemap library adds a map of the Earth for easy viewing
     m = Basemap(projection='cyl', llcrnrlat=latmin, urcrnrlat=latmax,
-                llcrnrlon=0, urcrnrlon=360, resolution='c', ax=ax)
+                llcrnrlon=lonmin, urcrnrlon=lonmax, resolution='c', ax=ax)
 
-    # creates a meshgrid from [0, 360] and [latmin, latmax] used to translate argument grid to lon and lat values 
     lon_vals = np.linspace(0, 360, grid.shape[1])
-    lat_vals = np.linspace(latmin, latmax, grid.shape[0])
+    lat_vals = np.linspace(-45, 45, grid.shape[0])
     lon_grid, lat_grid = np.meshgrid(lon_vals, lat_vals)
 
-    # remove values that are equal to -9999
     masked_grid = np.ma.masked_where(grid == -9999, grid)
 
     cs = m.pcolormesh(lon_grid, lat_grid, masked_grid, shading='auto', 
                       cmap='viridis', vmin=isomin, vmax=isomax)
 
-    # customization stuff
     m.drawcoastlines(linewidth=0.5)
     m.drawcountries(linewidth=0.5)
 
-    m.drawparallels(np.arange(-90, 91, 20), labels=[1, 0, 0, 0])  # Latitude lines
-    m.drawmeridians(np.arange(0, 361, 20), labels=[0, 0, 0, 1])   # Longitude lines
+    m.drawparallels(np.arange(-90, 91, 20), labels=[1, 0, 0, 0])  
+    m.drawmeridians(np.arange(0, 361, 20), labels=[0, 0, 0, 1])  
 
     cbar = m.colorbar(cs, location='right', pad="5%")
     cbar.set_label(label)
 
     ax.set_title(title)
 
-    # If statistics are provided, add them as annotations
     if stats:
         bias, rmsd, corr = stats
         textstr = '\n'.join((
@@ -61,7 +55,6 @@ def create_figure(grid, title, label, file_name, isomin, isomax, latmin, latmax,
             f"RMSD: {rmsd:.4f}",
             f"Correlation: {corr:.4f}"
         ))
-        # place the text box in the lower-left corner of the plot
         props = dict(boxstyle='round', facecolor='white', alpha=0.7)
         ax.text(0.05, 0.05, textstr, transform=ax.transAxes, fontsize=10,
                 verticalalignment='bottom', bbox=props)
@@ -70,25 +63,31 @@ def create_figure(grid, title, label, file_name, isomin, isomax, latmin, latmax,
     plt.close()
 
 def visualize_SPIRE_vs_CYGNSS():
-    with open('/data01/lpu/spire_snr_grid.pkl', 'rb') as f:
-        spire_snr_grid = pickle.load(f)
     with open('/data01/lpu/spire_reflectivity_grid.pkl', 'rb') as f:
         spire_refl_grid = pickle.load(f)
-    with open('/data01/lpu/cygnss_snr_grid.pkl', 'rb') as f:
-        cygnss_snr_grid = pickle.load(f)
     with open('/data01/lpu/cygnss_reflectivity_grid.pkl', 'rb') as f:
         cygnss_refl_grid = pickle.load(f)
 
     spire_refl_grid = spire_refl_grid[900:2700, :]
     refl_grid, stats = calculate_statistics(spire_refl_grid, cygnss_refl_grid)
-    create_figure(refl_grid, "SPIRE vs CYGNSS Reflectivity", "Abs. Diff. in Reflectivity (dBZ)", "figures/comp_reflectivity_map.png", 0, 0.015, -45, 45, stats) 
+    create_figure(refl_grid, "SPIRE vs CYGNSS Reflectivity (012024-062024)", "Abs. Diff. in Reflectivity (dBZ)", "figures/comp_reflectivity_map.png", 0, 0.06, 0, 360, -45, 45, stats) 
 
     spire_snr_grid = spire_snr_grid[900:2700, :]
     snr_grid, stats = calculate_statistics(spire_snr_grid, cygnss_snr_grid)
-    create_figure(snr_grid, "SPIRE vs CYGNSS SNR", "Abs. Diff. in SNR (dB)", "figures/comp_snr_map.png", 0, 10, -45, 45, stats) 
+    create_figure(snr_grid, "SPIRE vs CYGNSS SNR (012024-062024)", "Abs. Diff. in SNR (dB)", "figures/comp_snr_map.png", 0, 16, 0, 360, -45, 45, stats) 
 
+def visualize_SPIRE_vs_CGYNSS_area():
+    with open('/data01/lpu/spire_reflectivity_grid.pkl', 'rb') as f:
+        spire_refl_grid = pickle.load(f)
+    with open('/data01/lpu/cygnss_reflectivity_grid.pkl', 'rb') as f:
+        cygnss_refl_grid = pickle.load(f)
+
+    spire_refl_grid = spire_refl_grid[900:2700, :]
+    refl_grid, _ = calculate_statistics(spire_refl_grid, cygnss_refl_grid)
+    
+    create_figure(refl_grid, "SPIRE vs CYGNSS Reflectivity, Australia (01-06/2024)", "Abs. Diff. in Reflectivity (dBZ)", "figures/comp_reflectivity_australia_map.png", 0, 0.06, 100, 160, -45, -10) 
+    create_figure(refl_grid, "SPIRE vs CYGNSS Reflectivity, North/Central America (01-06/2024)", "Abs. Diff. in Reflectivity (dBZ)", "figures/comp_reflectivity_america_map.png", 0, 0.06, 220, 300, 10, 45)
 
 if __name__ == "__main__":
-    # i've ran into memory issues with this file, seemingly from caching too much
-    # closing the terminal and reopening seems to fix this for mes
-    visualize_SPIRE_vs_CYGNSS()
+    # visualize_SPIRE_vs_CYGNSS()
+    visualize_SPIRE_vs_CGYNSS_area()
