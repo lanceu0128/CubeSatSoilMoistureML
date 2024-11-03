1dce7e04fde980d1fc8451e06a0a77de6c709cc7
Lance <lanceu0128@gmail.com>
2024-11-03 16:15:22 -0500

    implemented CYGNSS quality control

--- make_grid_CYGNSS.py
+++ make_grid_CYGNSS.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import pickle
 from datetime import datetime
+from collections import Counter
 
 # Task: make a grid of CYGNSS SNR and Reflectivity data for comparison and validation of SPIRE data
 # very few comments because the logic is almost exactly the same as make_grid.py  
@@ -10,20 +11,23 @@ from datetime import datetime
 def read_file(file, grid_snr, grid_refl, grid_count_snr, grid_count_refl):
     nc_file = netCDF4.Dataset(file, "r", format="NETCDF4")
     
-    # lon ~ [0, 360], lat ~ [-45, 45]
-    # [:] needed to convert from netCDF4 variable arrays to float arrays
+    # Read variables from the netCDF file
     refl = nc_file.variables['reflectivity_peak'][:]
     snr = nc_file.variables['ddm_snr'][:]
     lon = nc_file.variables['sp_lon'][:]
     lat = nc_file.variables['sp_lat'][:]
+    quality = nc_file.variables['quality_flags'][:]
+    quality_2 = nc_file.variables['quality_flags_2'][:]
 
     x = (lon / 0.05).astype(int)
-    y = ((lat + 45) / 0.05).astype(int) # about half of the range
+    y = ((lat + 45) / 0.05).astype(int)  # about half of the range
 
     x = np.clip(x, 0, 7199)
 
-    valid_snr = ~np.ma.getmaskarray(snr) & ~np.isnan(snr)
-    valid_refl = ~np.ma.getmaskarray(refl) & ~np.isnan(refl)
+    valid_quality = ((((quality & 2048) == 0) | ((quality & 4096) == 0)) & ((quality_2 & 2048) == 0))
+    valid_quality = ((quality & 1) == 0) | ((quality_2 & 2048) == 0)
+    valid_snr = ~np.ma.getmaskarray(snr) & ~np.isnan(snr) & valid_quality
+    valid_refl = ~np.ma.getmaskarray(refl) & ~np.isnan(refl) & valid_quality & (snr <= 2)
 
     grid_snr[y[valid_snr], x[valid_snr]] += snr[valid_snr]
     grid_count_snr[y[valid_snr], x[valid_snr]] += 1
@@ -36,7 +40,7 @@ def read_file(file, grid_snr, grid_refl, grid_count_snr, grid_count_refl):
     return grid_snr, grid_refl, grid_count_snr, grid_count_refl
 
 if __name__ == "__main__":
-    directory = Path('/data01/jyin/CYGNSS/data/V3.2/2023/')
+    directory = Path('/data01/jyin/CYGNSS/data/V3.2/2024/')
 
     # data for comparison with SPIRE
     start_date = datetime.strptime("20240125", "%Y%m%d")
@@ -52,7 +56,7 @@ if __name__ == "__main__":
         if folder.is_dir():
             for file in folder.iterdir():
                 if file.is_file() and file.suffix == '.nc':
-                    date_str = filename.split("-e")[-1][:8]
+                    date_str = file.name.split("-e")[-1][:8]
                     file_date = datetime.strptime(date_str, "%Y%m%d")
 
                     if start_date <= file_date <= end_date:
